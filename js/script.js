54eef231a28ddc0bf636e1cbcf268fd825325b81
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-06-16 12:37:47 +0600

    galary and color section complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-  AOS.init();
+
+// aos animation code 
+
+AOS.init();
+
+// counter plugin default code 
 
   $('.counter').counterUp({
     delay: 10,
     time: 1000,
 });
+
+// slick slider default code 
+
+$('.slidertext').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+		
